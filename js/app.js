5eb04a7ae09f174ca1ae7cfe5803ddcd5a89f97b
Julien Fuchs <jfuchs@noussommeslegion.com>
2015-12-07 14:44:22 +0100

    correction 07/12/15 + dev du switch pour les cas

--- js/app.js
+++ js/app.js
@@ -8,6 +8,72 @@ $(document).ready(function() {
 
 
     getDonAmount();
+
+    // SWITCH
+    function killSwitch() {
+        if( parseInt($(window).width()) <= 565 ) {
+            $(window).off('mousemove').off('mouseup');
+            $('.section-case').removeClass('init').addClass('no-switch').find('.trigger-move').off('click').off('mousedown');
+        } else if( $('.section-case').hasClass('no-switch')) {
+            $('.section-case').removeClass('no-switch');
+            initSwitch();
+        }
+    }
+    $(window).resize(function() {
+        killSwitch();
+    });
+    killSwitch();
+
+    function initSwitch() {
+        $(window).resize(function() {
+            $('.section-case').each(function() {
+                var $caseTrue = $(this).find('.section-case-true'),
+                    $caseFalse = $(this).find('.section-case-false');
+
+                // Update height
+                var _h = $caseTrue.outerHeight(true) > $caseFalse.outerHeight(true) ? $caseTrue.outerHeight(true) : $caseFalse.outerHeight(true);
+
+                $(this).css({ height : _h}).addClass('init');
+
+                if( $caseTrue.attr('style') != null && $caseTrue.offset().left > 0 ) {
+                    TweenMax.set($caseTrue, { x : $(window).width() });
+                }
+            });
+        }).trigger('resize');
+
+        $('.section-case').find('.trigger-move').on('click', function(e) {
+            e.preventDefault();
+        }).on('mousedown touchstart', function(e) {
+            e.preventDefault();
+            var $parent = $(this).parents('.section-case-true');
+
+            $(window).on('mousemove touchmove', function(e) {
+                e.preventDefault();
+
+                var pageX = (e.type.toLowerCase() === 'mousemove')
+                    ? e.pageX
+                    : e.originalEvent.touches[0].pageX;
+                TweenMax.set($parent, { x : pageX });
+            });
+
+            $(window).on('mouseup touchend', function(e) {
+                e.preventDefault();
+
+                var pageX = (e.type.toLowerCase() === 'mouseup')
+                    ? e.pageX
+                    : e.originalEvent.changedTouches[0].pageX;
+
+
+                if( pageX > $(window).width() / 2 ) {
+                    TweenMax.to($parent, 0.3, { x : $(window).width() });
+                } else {
+                    TweenMax.to($parent, 0.3, { x : 0 });
+                }
+
+                $(window).off('mousemove').off('mouseup');
+            });
+        });
+    }
 })
 
 function getDonAmount() {
